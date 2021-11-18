30fcfd94982c48ef22cdc8672a656ff2a20a41b8
JianXF <3109441270@qq.com>
2021-11-18 22:30:03 +0800

    测试程序

--- test.cpp
+++ test.cpp
@@ -1,8 +1,10 @@
 #include"HuffmanCoding.hpp"
 #include"Document.cpp"
 using namespace std;
+
 int main(){
     string filePath;
+    cout<<"输入文件路径或当前路径下文件名:";
     cin>>filePath;
     const char* path=filePath.c_str();
     Document test(path);
