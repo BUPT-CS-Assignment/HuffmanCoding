8297e2f07188c47224325e9f27a8cda135ab5b0a
JianXF <3109441270@qq.com>
2021-12-01 01:34:10 +0800

    New Version

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: G:/Coding/CProject/DataStructure/HuffmanCoding/HuffmanCoding
+# Build directory: G:/Coding/CProject/DataStructure/HuffmanCoding/HuffmanCoding/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
